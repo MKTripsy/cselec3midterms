88aa5c8794c61991520081d867daa4bb6b19fbc4
MKTripsy <janchristiansentripolca@gmail.com>
2025-06-21 23:33:10 +0800

    added sql file

---
+++ sql/.sql
@@ -0,0 +1,6 @@
+INSERT INTO students (name, email, course)
+VALUES ('John Max', 'johnmax@email.com', 'Computer Science'),
+('John Doe', 'johndoe@email.com', 'Architecture'),
+('Jane Doe', 'janedoe@email.com', 'Animation'),
+('Jane Max', 'janemax@email.com', 'Graphic Design'),
+('Max Doe', 'maxdoe@email.com', 'Graphic Design');
